33291ed47b53fac2dcdbb97f264de978becf07bf
Wahyu Rismawan <wahyu@badr-interactive.com>
2015-11-24 00:52:42 +0700

    first commit

---
+++ crawler.py
@@ -0,0 +1,44 @@
+import urllib
+from mylib import *
+from bs4 import BeautifulSoup
+import json
+
+def get_tafsir(surat, ayat):
+	url = 'http://quran.ksu.edu.sa/interface.php?ui=pc&do=tafsir&author=indonesian&sura='+str(surat)+'&aya='+str(ayat);
+	html = urllib.urlopen(url).read()
+	parsed_html = BeautifulSoup(html, 'lxml')
+	return parsed_html.body.find('div', attrs={'class':'tafheem_comments'}).text
+
+def do_crawl(surats):
+	result = []
+	for surat in surats:
+		for ayat in xrange(1,surat['ayat']):
+			print "*********** QS {0} - Ayat {1}".format(surat['nama'], ayat)
+			t = { 'surat' : 1, 'ayat' : ayat, 'tafsir' : get_tafsir(surat['no'],ayat) }
+			print t['tafsir']
+			result.append(t)
+
+	writeJSON(json.dumps(result), "result.json")
+
+def reformat_data_surat():
+	surats = CSV2List("surat.csv")
+	surat_list = []
+	for surat in surats:
+		surat_obj = {
+			"no" : int(surat[0]),
+			"nama" : surat[1],
+			"arti" : surat[2],
+			"ayat" : int(surat[3]),
+			"tempat_turun" : surat[4],
+			"urutan_turun" : int(surat[5])
+		}
+		print surat_obj
+		surat_list.append(surat_obj)
+
+	writeJSON(json.dumps(surat_list), "surat.json")
+
+def __main__():
+	surats = readJSON("surat.json")
+	do_crawl(surats)
+
+__main__()
---
+++ mylib.py
@@ -0,0 +1,61 @@
+__author__ = 'wrismawan'
+import csv
+
+def CSV2List(filename):
+    with open(filename, 'rb') as f:
+        reader = csv.reader(f)
+        new_list = list(reader)
+    return new_list
+
+
+def readFromCSV(filename):
+    _file = open(filename)
+    __file = open(filename)
+    lines = csv.reader(_file)
+    rows = csv.reader(__file)
+    data = {}
+
+    row_count = sum(1 for row in rows)
+    data = [[] for i in range(row_count)]
+    i = 0
+    for row in lines:
+        cells = row
+        cols = len(cells)
+        for cell in range(cols):
+            if ('.' in cells[cell]):
+                num = float(cells[cell])
+            else:
+                num = int(cells[cell])
+            data[i].append(num)
+        i += 1
+
+    return data
+
+
+def writeToCSV(listData, fileName):
+    file_data = fileName
+    with open(file_data, 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        for i in listData:
+            writer.writerow(i, )
+    return file_data
+
+
+def writeJSON(json, fileName):
+    target = open(fileName, 'wb')
+    target.write(json)
+    return target
+
+
+def readJSON(fileName):
+    import json
+    with open(fileName) as file:
+        return json.load(file)
+
+def JSON2Matrix(listInput):
+    result = []
+    for key, value in listInput.iteritems():
+        l = [[key], value]
+        row = [item for sublist in l for item in sublist]
+        result.append(row)
+    return result

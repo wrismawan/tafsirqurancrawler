f99222e9157b9d63d3d9ab3e8d594afeab8f68e0
Wahyu Rismawan <wahyu@badr-interactive.com>
2015-11-24 11:03:29 +0700

    backing up result tafsir when request error

--- crawler.py
+++ crawler.py
@@ -3,26 +3,25 @@ from mylib import *
 from bs4 import BeautifulSoup
 import json
 
+REQUEST_ERROR = 'request_error'
+PARSED_ERROR = 'parsed_error'
+
+
 def get_tafsir(surat, ayat):
 	url = 'http://quran.ksu.edu.sa/interface.php?ui=pc&do=tafsir&author=indonesian&sura='+str(surat)+'&aya='+str(ayat);
-	html = urllib.urlopen(url).read()
+	try:
+		html = urllib.urlopen(url).read()
+	except Exception, e:
+		return REQUEST_ERROR
+
 	parsed_html = BeautifulSoup(html, 'lxml')
+
 	try:
 		text_tafsir = parsed_html.body.find('div', attrs={'class':'tafheem_comments'}).text
 	except Exception, e:
-		text_tafsir = "error"
+		text_tafsir = PARSED_ERROR
+
 	return text_tafsir
-
-def do_crawl(surats):
-	result = []
-	for surat in surats:
-		for ayat in xrange(1,surat['ayat']+1):
-			print "*********** QS {0} - Ayat {1}".format(surat['nama'], ayat)
-			t = { 'surat' : 1, 'ayat' : ayat, 'tafsir' : get_tafsir(surat['no'],ayat) }
-			print t['tafsir']
-			result.append(t)
-
-	writeJSON(json.dumps(result), "result.json")
 
 def reformat_data_surat():
 	surats = CSV2List("surat.csv")
@@ -36,13 +35,35 @@ def reformat_data_surat():
 			"tempat_turun" : surat[4],
 			"urutan_turun" : int(surat[5])
 		}
-		print surat_obj
 		surat_list.append(surat_obj)
 
 	writeJSON(json.dumps(surat_list), "surat.json")
 
+def save_result(surat, ayat, result):
+	fileName = "result_{0}_{1}.json"
+	writeJSON(json.dumps(result), fileName)
+
+def do_crawl(surats):
+	result = []
+	for surat in surats:
+		for ayat in xrange(1,surat['ayat']+1):
+			tafsir = get_tafsir(surat['no'],ayat)
+			
+			print "*********** QS {0} - Ayat {1} **********".format(surat['nama'], ayat)
+			print tafsir
+			
+			# if request error, save current tafsir result (backup)
+			if (tafsir == REQUEST_ERROR):
+				save_result(surat=surat['nama'], ayat=ayat, result=tafsir)
+				print "****** REQUEST ERROR ******"
+			
+			result.append({ 'surat' : 1, 'ayat' : ayat, 'tafsir' :  tafsir })
+
+			
+	save_result(surat=surat['nama'], ayat=ayat, result=tafsir)
+	
 def __main__():
 	surats = readJSON("surat.json")
-	do_crawl(surats)
+	do_crawl(surats[:30])
 
 __main__()
